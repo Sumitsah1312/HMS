13e9caddeeb878fa02d0bcedbd1e82a65ac90362
sumit shah <sumit.shah@mashvirtual.in>
2026-03-10 00:32:04 +0530

    cc

--- Backend/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59ec4c117e62e0b73751a4c593ca7de3c68b9281")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1ee8d6a98abadfcc8a8ebffec691df05cfb5ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
