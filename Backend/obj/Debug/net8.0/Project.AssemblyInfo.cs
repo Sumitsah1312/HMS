59ec4c117e62e0b73751a4c593ca7de3c68b9281
sumit shah <sumit.shah@mashvirtual.in>
2026-03-09 23:54:36 +0530

    Structure Changes Add Frontend

--- Backend/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2bbfa35088f713d25b580a3266b943ab90d622c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c3b8f88b0b59bfa97ca1ebe093b58231ae07d86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
