f86a4a01ad30d2e58bf701892f9523f6a752f83e
Sumitsah1312 <97793625+Sumitsah1312@users.noreply.github.com>
2026-03-30 01:22:57 +0530

    dd

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -104,11 +104,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Env/ironment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
 
